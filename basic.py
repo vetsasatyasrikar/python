ab11ca6a50de3e47b7026d09351add5e1ed8003c
vetsasatyasrikar <srikarvetsa2007@gmail.com>
2026-02-11 22:28:22 +0530

    add

--- basic.py
+++ basic.py
@@ -1,3 +1,39 @@
-print("hello world ", 10 + 10)
-print("max")
-print("my name is Srikar")
+# print("hello world ", 10 + 10)
+# print("max")
+# print("my name is Srikar")
+# a = 150
+# b = 1.55
+# c = a+b
+# print(type(a))
+# print(type(b))
+# print(c)
+# print(type(c))
+# num_float = float("1.55")
+# message = str(55)
+# print(message)
+# print(num_float)
+# char_a = 'S'
+# char_b = 'V'
+# c = 79
+# ascii_c = chr(c)
+# ascii_a = ord(char_a)
+# ascii_b = ord(char_b)
+# print(ascii_c)
+# print(ascii_b)
+# print(ascii_a)
+a = 99
+b = 55
+c = a+b
+d = a*b
+x = a/b
+print(d)
+print(x)
+print(c)
+a = 99
+b = 55
+c = a%b
+d = a**b
+x = a//b
+print(d)
+print(x)
+print(c)
